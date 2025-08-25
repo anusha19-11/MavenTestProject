ae32ba0473da4c74b5403cd2942ca93e2cc43d6f
anusha19-11 <thangellaanusha19@gmail.com>
2025-08-25 12:20:46 +0530

    Add files via upload
    first commit

---
+++ src/test/java/mavendemo/testing/MavenDemo.java
@@ -0,0 +1,15 @@
+package mavendemo.testing;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class MavenDemo {
+	@Test
+	public void setUp() {
+	WebDriver driver = new ChromeDriver();
+	driver.get("https://www.google.com/");
+	driver.manage().window().maximize();
+	}
+
+}
